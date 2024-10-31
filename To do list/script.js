a8afdb1355dd12511e861a9e2a934b62b226744a
Areefa-Cader <areefacader12345@gmail.com>
2024-11-01 01:20:09 +0530

    todolist ui completed

---
+++ To do list/script.js
@@ -0,0 +1,6 @@
+const checkBox = document.getElementById('checkBox');
+const task = document.getElementById('task');
+const inputTask = document.querySelector('.input-task');
+const todolist = document.querySelector('.todolist');
+const pendingNumber = document.querySelector('.pending-number');
+
